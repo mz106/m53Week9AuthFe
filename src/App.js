d9972a4969f0e35dffc9be910461abc861a662dd
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-01-09 15:31:07 +0000

    enhancement: signup added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import LogOrSign from "./components/logOrSign/LogOrSign";
 
 function App() {
   return (
     <div>
       <Header />
+      <LogOrSign />
       <Footer />
     </div>
   );
